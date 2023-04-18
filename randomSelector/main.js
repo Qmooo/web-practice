8db618acf12ea8e99b813051a6709b2c2e0ad3c0
Alex Chou <zoh92117@gmail.com>
2023-04-18 13:27:08 +0800

    Update for work

--- randomSelector/main.js
+++ randomSelector/main.js
@@ -3,7 +3,7 @@ $(function(){
     $("input").on("click",function(){
         var numberOfListItem = $("li").length;
         var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
-        var images = ["/randomSelector/1.jpg","/randomSelector/2.jpg","/randomSelector/3.jpg","/randomSelector/5.jpg"];
+        var images = ["randomSelector/1.jpg","randomSelector/2.jpg","randomSelector/3.jpg","randomSelector/5.jpg"];
         while(randomChildNumber == prev){
             randomChildNumber = Math.floor(Math.random()*numberOfListItem);    
         }
